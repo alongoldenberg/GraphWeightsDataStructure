8b7970f17844fcb8f5f7ff3f473e11f723944912
Alon Goldenberg <alon.goldenberg@securingsam.com>
2021-06-10 16:31:06 +0300

    more functions

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -18,7 +18,7 @@ public class Graph {
         for(Node node: nodes){
             HeapNode heapNode = new HeapNode(node.node_id, node.weight);
             weightHeap.insert(heapNode);
-            
+
         }
     }
 
@@ -45,17 +45,53 @@ public class Graph {
     }
 
     public class HashTable{
-        private Node[] hashTable;
-        private long p = 10^9 + 9;
-        private long a;
-        private long b;
+        private HashCell[] hashTable;
+        private int p = 10^9 + 9;
+        private int n;
+        private int a;
+        private int b;
 
         public HashTable(Node[] graph) {
+            this.n = graph.length;
+            this.hashTable = new HashCell[n];
             Random r = new Random();
-            this.a = (long) (r.nextDouble() * p + 1);
-            this.b = (long) (r.nextDouble() * p);
+            this.a = (int) (r.nextDouble() * p + 1);
+            this.b = (int) (r.nextDouble() * p);
+            for (Node node: graph){
+                insert(node);
+            }
+        }
+
+        private int hash(int node_id){
+            return ((a*node_id + b) % p) % n;
+        }
+        
+        private void insert(Node node){
+            
         }
     }
+    
+    private class HashCell{
+        public int node_id;
+        public HeapNode heapPointer;
+        public AdjacencyList nodeAdjencies;
+        public HashCell next;
+
+        public HashCell(int node_id, HeapNode heapPointer, AdjacencyList nodeAdjencies) {
+            this.node_id = node_id;
+            this.heapPointer = heapPointer;
+            this.nodeAdjencies =  nodeAdjencies;
+        }
+
+        public HashCell getNext() {
+            return next;
+        }
+
+        public void setNext(HashCell next) {
+            this.next = next;
+        }
+    }
+    
 
     public class AdjacencyList{
 
